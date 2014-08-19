6b8f33c65eb0567e468358b5f3dced8363c1825e
Stefan Boonstra <stefanboonstra@hotmail.com>
2014-08-19 10:46:46 +0200

    Slideshow container calculation now rounds to full pixels instead of taking floor

--- slideshow.php
+++ slideshow.php
@@ -3,7 +3,7 @@
  Plugin Name: Slideshow
  Plugin URI: http://wordpress.org/extend/plugins/slideshow-jquery-image-gallery/
  Description: The slideshow plugin is easily deployable on your website. Add any image that has already been uploaded to add to your slideshow, add text slides, or even add a video. Options and styles are customizable for every single slideshow on your website.
- Version: 2.2.21
+ Version: 2.2.22
  Requires at least: 3.5
  Author: StefanBoonstra
  Author URI: http://stefanboonstra.com/
@@ -22,7 +22,7 @@
 class SlideshowPluginMain
 {
 	/** @var string $version */
-	static $version = '2.2.21';
+	static $version = '2.2.22';
 
 	/**
 	 * Bootstraps the application by assigning the right functions to
